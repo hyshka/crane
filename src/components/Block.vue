493d61a55e6356a33c409e750adb8334f0305515
Bryan Hyshka <bryan@hyshka.com>
2016-12-21 16:34:00 -0700

    refactor contenteditable

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -5,7 +5,7 @@
       contenteditable
       @input="buildBlock($event)"
       @keyup.esc="blurContent"
-      @keydown.shift.enter="blurContent">{{ title }}</div>
+      @keydown.shift.enter="blurContent">{{ formattedTitle }}</div>
     </textarea>
   </div>
 </template>
@@ -26,13 +26,76 @@ export default {
       focused: false,
     };
   },
+  computed: {
+    formattedTitle() {
+      let title = this.title.trim();
+      title = title.replace(/\n+\s+\n+/g, '\n\n');
+      title = title.replace(/\n\n+/g, '\n\n');
+      title = title.replace(/\n/g, '<br>');
+      title = title.replace(/\s+/g, ' ');
+
+      return title;
+    },
+  },
   methods: {
     ...mapActions([
       'updateBlock',
     ]),
     buildBlock(event) {
+      let title = event.target.innerHTML;
+
+      // Convert `&amp;` to `&`.
+      title = title.replace(/&amp;/gi, '&');
+
+      // Replace spaces.
+      title = title.replace(/&nbsp;/gi, ' ');
+      title = title.replace(/\s+/g, ' ');
+
+      // Remove "<b>".
+      title = title.replace(/<b>/gi, '');
+      title = title.replace(/<\/b>/gi, '');
+
+      // Remove "<strong>".
+      title = title.replace(/<strong>/gi, '');
+      title = title.replace(/<\/strong>/gi, '');
+
+      // Remove "<i>".
+      title = title.replace(/<i>/gi, '');
+      title = title.replace(/<\/i>/gi, '');
+
+      // Remove "<em>".
+      title = title.replace(/<em>/gi, '');
+      title = title.replace(/<\/em>/gi, '');
+
+      // Remove "<u>".
+      title = title.replace(/<u>/gi, '');
+      title = title.replace(/<\/u>/gi, '');
+
+      // Tighten up "<" and ">".
+      title = title.replace(/>\s+/g, '>');
+      title = title.replace(/\s+</g, '<');
+
+      // Replace "<br>".
+      title = title.replace(/<br>/gi, '\n');
+
+      // Replace "<div>" (from Chrome).
+      title = title.replace(/<div>/gi, '\n');
+      title = title.replace(/<\/div>/gi, '');
+
+      // Replace "<p>" (from IE).
+      title = title.replace(/<p>/gi, '\n');
+      title = title.replace(/<\/p>/gi, '');
+
+      // No more than 2x newline, per "paragraph".
+      title = title.replace(/\n\n+/g, '\n\n');
+
+      // Whitespace before/after.
+      title = title.trim();
+
+      console.log('block', title);
+
       const mutation = {
-        title: event.target.textContent,
+        title,
         blockIndex: this.blockIndex,
         rowIndex: this.rowIndex,
       };
